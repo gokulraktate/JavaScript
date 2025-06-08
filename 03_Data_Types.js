2e74ba03ff920596fa0dbb71d6e0eee141026a44
gokulraktate <gokulsanjayraktate@users.github.com>
2025-06-08 14:46:50 +0530

    DataTypes

---
+++ 03_Data_Types.js
@@ -0,0 +1,23 @@
+ let name = "Gokul"     //string
+ let age = 21           //numeric/number
+ let flag = true        //boolean
+ let state;             //undefined
+ let value = null       //Null and null is an object
+
+
+
+ console.log(typeof(name));
+ console.log(typeof(age));
+ console.log(typeof(flag));
+ console.log(typeof(state));
+ console.log(typeof(value));
+
+ 
+
+//  output
+
+// string
+// number
+// boolean
+// undefined
+// object
