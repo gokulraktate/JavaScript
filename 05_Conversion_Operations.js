a266b5152d28d98d39d8638c6739dbd0c5e8a92d
gokulraktate <gokulsanjayraktate@users.github.com>
2025-06-08 15:24:23 +0530

    Conversion Operations

---
+++ 05_Conversion_Operations.js
@@ -0,0 +1,24 @@
+let value=3
+let negValue= -value
+
+console.log(negValue);     //-3
+
+
+let str1= "Hello "
+let str2= "Gokul"
+let str3= str1 + str2
+
+console.log(str3);
+
+
+
+console.log("1" + 2);      //12
+console.log("1" + "2");    //12
+console.log(1 + "2");      //12
+console.log("1" + 2 + 2);      //122
+console.log(1 + 2 + "2");      //32
+console.log(1 + "2" + 2);      //122
+
+console.log(true);         //true
+console.log(+true);        //1
+console.log(-true);        //-1
