24799b379e151d48fe8991ecb29c6713887b89de
gokulraktate <gokulsanjayraktate@users.github.com>
2025-06-20 16:06:51 +0530

    Object in JS

---
+++ 16_Object_3.js
@@ -0,0 +1,27 @@
+const course={
+    name:"Js Course",
+    price:999,
+    teacher:"Hitesh"
+}
+
+
+// console.log(course.teacher);
+const {teacher}=course                //this is use to minimise the code if any object value used multiple time
+console.log(teacher);         
+
+
+// Json sample
+
+// {
+//     name:"Gokul",
+//     price:1929,
+//     age:19
+// }
+
+// or
+
+[
+    {},
+    {},
+    {},
+]
