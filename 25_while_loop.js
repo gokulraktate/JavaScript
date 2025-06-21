a6e002d859eb9b12acdfaa2868570b6a1accc325
gokulraktate <gokulsanjayraktate@users.github.com>
2025-06-21 15:30:23 +0530

    While and Do While Loop

---
+++ 25_while_loop.js
@@ -0,0 +1,38 @@
+// while loop
+
+// syntax
+
+// while (condition) {
+    
+// }
+
+
+let val=4
+
+while(val>=0){
+    console.log(val);
+    val=val-1;
+}
+
+
+let myarr=[1,2,3,4,5,6,7,8,9,10]
+let i=0
+while(i!=myarr.length-1){
+    console.log(myarr[i]);
+    i++;
+}
+
+
+
+// do while
+
+
+// do {
+    
+// } while (condition);
+
+let score=1;
+do {
+    console.log("The score is ",score);
+    score++;
+} while (score<=10);
